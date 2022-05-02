24eaef4b83901cc46cce43ddbc94458c48aa7be1
Eugene Kravec <krava255@gmail.com>
2022-05-02 09:24:38 +0300

    Add JSON with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './App.css';
+import { Footer } from './Footer';
+
+const data = {
+  firstName: 'Eugene',
+  position: 'Junior React developer',
+  mobile: '0800500609'
+
 }
+
+const App = () => (
+  <div className="App">
+    <Footer copyright='Eugene' />
+    <h1>{data.firstName}</h1>
+    <h2>{data.position}</h2>
+    <h2>{data.mobile}</h2>
+  </div>
+)
 
 export default App;
